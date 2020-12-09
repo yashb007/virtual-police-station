5a8aa4848ce43ec23585cccaed9be3fe7da2df56
yashb007 <bansaly37@gmail.com>
2020-12-09 10:49:42 +0530

    build

---
+++ client/build/precache-manifest.aab4ff985eb3272e16696ab266b1dbad.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c6d85929eb94709c6e76647a36fef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "398d12be5d061954cef5",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "4d443532a0d08f8f7678",
+    "url": "/static/css/main.a6bacedb.chunk.css"
+  },
+  {
+    "revision": "398d12be5d061954cef5",
+    "url": "/static/js/2.7d36eea2.chunk.js"
+  },
+  {
+    "revision": "a3ed4ebb86934179471748febfa8d538",
+    "url": "/static/js/2.7d36eea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d443532a0d08f8f7678",
+    "url": "/static/js/main.f86f2b93.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "4055d9f048e1542a4c867a243c8cb381",
+    "url": "/static/media/EcYTq93px20ptlPRSq1C.4055d9f0.png"
+  },
+  {
+    "revision": "fb5dacb7195fba8e119940d1bb35666d",
+    "url": "/static/media/black-1072366_1920.fb5dacb7.jpg"
+  },
+  {
+    "revision": "2088b3f22bb6bdb695b15d06eabee64d",
+    "url": "/static/media/clinic-2921858_1920.2088b3f2.jpg"
+  },
+  {
+    "revision": "7a9b924b7626c5529273697c619ba36f",
+    "url": "/static/media/croods.7a9b924b.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "f823554424246326520f75f60a58678f",
+    "url": "/static/media/handshake-3139227_640.f8235544.jpg"
+  },
+  {
+    "revision": "6d5b65615337031d7d0e4fb48b498899",
+    "url": "/static/media/happy-bunch (2).6d5b6561.png"
+  },
+  {
+    "revision": "d4c6b4fd93bc0d95ce0567db57736368",
+    "url": "/static/media/happy-bunch.d4c6b4fd.png"
+  },
+  {
+    "revision": "b1a3d33a4a1c9e6e2d99eaf588d00804",
+    "url": "/static/media/heart-care-1040250_1280.b1a3d33a.jpg"
+  },
+  {
+    "revision": "6ef2dccd7318932d970c73640e347f7f",
+    "url": "/static/media/hos_blue.6ef2dccd.png"
+  },
+  {
+    "revision": "bed304b34df0e6b14da5720cf7876f0e",
+    "url": "/static/media/humaaans.bed304b3.png"
+  },
+  {
+    "revision": "b25ebd2db4b9791ebe7dcd921e0fdcb9",
+    "url": "/static/media/humaans1.b25ebd2d.png"
+  },
+  {
+    "revision": "af49be1e09eb6d58e2c158b3beab3045",
+    "url": "/static/media/id.af49be1e.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "22ec17e6d3b058cf14788d85042ade5b",
+    "url": "/static/media/lab.22ec17e6.jpg"
+  },
+  {
+    "revision": "f66fe5ed13cb3862d91f591f7531d4a0",
+    "url": "/static/media/lab.f66fe5ed.png"
+  },
+  {
+    "revision": "fd6374e0621b1d80cc3d99d2a5faf5ab",
+    "url": "/static/media/medical-563427_640.fd6374e0.jpg"
+  },
+  {
+    "revision": "b606dc4feb634ee19b8520493f120f4c",
+    "url": "/static/media/nature-2608599_1920.b606dc4f.jpg"
+  },
+  {
+    "revision": "09d6e277df084afda217b5b4d53f8538",
+    "url": "/static/media/one.09d6e277.png"
+  },
+  {
+    "revision": "3a19a02fd6c0e1f0d21b5bb65ae62d0c",
+    "url": "/static/media/questions-2245264_1280.3a19a02f.jpg"
+  },
+  {
+    "revision": "453394eaab6247302f18100e78d57963",
+    "url": "/static/media/shiny-happy-people.453394ea.png"
+  },
+  {
+    "revision": "977336a515e1684266a54278efcff7a8",
+    "url": "/static/media/shiny-happy-people.977336a5.png"
+  },
+  {
+    "revision": "1acecd8618b7d31ed1f32fd7ac6bbbb5",
+    "url": "/static/media/squad-car-1209719_1920.1acecd86.jpg"
+  },
+  {
+    "revision": "3382351758b9dbdf3de819d608eb75b4",
+    "url": "/static/media/woman-3169726_1920.33823517.jpg"
+  }
+]);
